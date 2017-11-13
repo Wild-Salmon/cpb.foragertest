5b49b27da4ac3d71c1805f2aaaad6ee042320463
Wild-Salmon <vince.bothorel@gmail.com>
2017-11-13 12:35:16 +0100

    testchanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
-import { FlexLayoutModule } from "@angular/flex-layout"; 
+//import { FlexLayoutModule } from "@angular/flex-layout"; 
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './services/in-memory-data-service';
